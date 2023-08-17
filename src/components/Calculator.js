805c51b703d2e59f250f45307fbc3f17803fdd0c
Purshotam Bohra <67221507+PBJI@users.noreply.github.com>
2023-08-17 18:10:39 +0530

    Separating Calculator.js and its Button Components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/Calculator.css';
 import calculate from '../logic/Calculate';
+import generate from './ButtonLayout';
 
 const calLayout = {
   'row-first':['AC', '+/-', '%', '÷'], 
@@ -40,43 +41,5 @@ const Calculator = () => {
     </>
   );
 };
-
-function generateButton(buttonName, onClickFunction) {
-  return (
-    <button type="button" name={buttonName} onClick={onClickFunction}>
-      {buttonName}
-    </button>
-  );
-}
-
-function generateRow(rowName, buttons, onClickFunction) {
-  if (rowName === 'row-fifth') {
-    return (
-      <article className={`math-row ${rowName}`}>
-        <div>
-          {generateButton(buttons[0], onClickFunction)}
-        </div>
-        <div>
-          {buttons.slice(1).map((button) => generateButton(button, onClickFunction))}
-        </div>
-      </article>
-    );
-  }
-  return (
-    <article className={`math-row ${rowName}`}>
-      {buttons.map((button) => generateButton(button, onClickFunction))}
-    </article>
-  );
-}
-
-function generateCalLayout(calLayout, onClickFunction) {
-  return (
-    <article className="math-functions">
-      {Object.keys(calLayout).map((rowName) =>
-        generateRow(rowName, calLayout[rowName], onClickFunction)
-      )}
-    </article>
-  );
-}
 
 export default Calculator;

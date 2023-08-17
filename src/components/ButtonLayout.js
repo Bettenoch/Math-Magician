917b70e06d93f03a4b976cb380938d0b3e4fcda3
Purshotam Bohra <67221507+PBJI@users.noreply.github.com>
2023-08-17 18:11:29 +0530

    Create ButtonLayout.js

---
+++ src/components/ButtonLayout.js
@@ -0,0 +1,37 @@
+function generateButton(buttonName, onClickFunction) {
+  return (
+    <button type="button" name={buttonName} onClick={onClickFunction}>
+      {buttonName}
+    </button>
+  );
+}
+
+function generateRow(rowName, buttons, onClickFunction) {
+  if (rowName === 'row-fifth') {
+    return (
+      <article className={`math-row ${rowName}`}>
+        <div>
+          {generateButton(buttons[0], onClickFunction)}
+        </div>
+        <div>
+          {buttons.slice(1).map((button) => generateButton(button, onClickFunction))}
+        </div>
+      </article>
+    );
+  }
+  return (
+    <article className={`math-row ${rowName}`}>
+      {buttons.map((button) => generateButton(button, onClickFunction))}
+    </article>
+  );
+}
+
+export default function generateBTLayout(calLayout, onClickFunction) {
+  return (
+    <article className="math-functions">
+      {Object.keys(calLayout).map((rowName) =>
+        generateRow(rowName, calLayout[rowName], onClickFunction)
+      )}
+    </article>
+  );
+}

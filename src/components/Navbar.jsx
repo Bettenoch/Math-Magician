d461e391490683cd90740e27f638cff259bc64aa
Bett Enock <bettenock968@gmail.com>
2023-08-22 14:37:56 -0700

    Add navbar with links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Navbar.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="nav-items">
+    <div className="logo">Math Magicians</div>
+    <ul className="nav-links">
+      <li><Link className="navlink" to="/">Home</Link></li>
+      <li><Link className="navlink" to="/Calculator">Calculator</Link></li>
+      <li><Link to="/Quotes">Quotes</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
